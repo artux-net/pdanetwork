6d670b92d2207cec55658cae3253a2a9a5f3a6b5
Semyon Kulikov <senich.99@gmail.com>
2021-11-13 03:02:01 +0300

    RM 70 Refactored regexps, added returning invalid characters

--- src/main/java/net/artux/pdanetwork/controller/UserValidator.java
+++ src/main/java/net/artux/pdanetwork/controller/UserValidator.java
@@ -7,6 +7,9 @@ import net.artux.pdanetwork.repository.MemberRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.stream.Stream;
 
 @Component
@@ -16,9 +19,9 @@ public class UserValidator {
     private final MemberRepository memberRepository;
 
     private static final String EMAIL_VALIDATION_REGEX = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-    private static final String LOGIN_VALIDATION_REGEX = "^[a-zA-Z][a-zA-Z0-9-_.]+$";
-    private static final String NAME_VALIDATION_REGEX = "^[а-яА-ЯёЁa-zA-Z]+$";
-    private static final String PASSWORD_VALIDATION_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)([a-zA-Z\\d])*$";
+    private static final String LOGIN_VALIDATION_REGEX = "^[a-zA-Z0-9-_.]+$";
+    private static final String NAME_VALIDATION_REGEX = "^[A-Za-z\u0400-\u052F\u2DE0-\u2DFF\uA640-\uA69F']*$";
+    private static final String PASSWORD_VALIDATION_REGEX = "^[A-Za-z\\d!@#$%^&*()_+№\";:?><\\[\\]{}]*$";
 
     public Status checkUser(RegisterUser user) {
         return Stream.of(checkLogin(user.getLogin()),
@@ -35,8 +38,10 @@ public class UserValidator {
         if (!StringUtils.hasText(login)) {
             return new Status(false, "Логин не может быть пустым.");
         }
-        if (!login.matches(LOGIN_VALIDATION_REGEX)) {
-            return new Status(false, "Логин содержит запрещённые символы.");
+        Collection<String> defectSymbols = checkStringSymbolsByRegexp(login, LOGIN_VALIDATION_REGEX);
+        if (!defectSymbols.isEmpty()) {
+            return new Status(false,
+                    "Логин содержит запрещённые символы: " + String.join(", ", defectSymbols));
         }
         if (login.length() < 4 || login.length() > 16) {
             return new Status(false, "Логин должен содержать не менее 4 и не более 16 символов.");
@@ -51,8 +56,10 @@ public class UserValidator {
         if (!StringUtils.hasText(name)) {
             return new Status(false, "Имя не может быть пустым.");
         }
-        if (!name.matches(NAME_VALIDATION_REGEX)) {
-            return new Status(false, "Имя содержит запрещённые символы.");
+        Collection<String> defectSymbols = checkStringSymbolsByRegexp(name, NAME_VALIDATION_REGEX);
+        if (!defectSymbols.isEmpty()) {
+            return new Status(false,
+                    "Имя содержит запрещённые символы: " + String.join(", ", defectSymbols));
         }
         if (name.length() < 2 || name.length() > 16) {
             return new Status(false, "Имя должно содержать не менее 2 и не более 16 символов.");
@@ -62,13 +69,15 @@ public class UserValidator {
 
     private Status checkNickname(String nickname) {
         if (!StringUtils.hasText(nickname)) {
-            return new Status(false, "Кличка не может быть пустой.");
+            return new Status(false, "Прозвище не может быть пустой.");
         }
-        if (!nickname.matches(NAME_VALIDATION_REGEX)) {
-            return new Status(false, "Кличка содержит запрещённые символы.");
+        Collection<String> defectSymbols = checkStringSymbolsByRegexp(nickname, NAME_VALIDATION_REGEX);
+        if (!defectSymbols.isEmpty()) {
+            return new Status(false,
+                    "Прозвище содержит запрещённые символы: " + String.join(", ", defectSymbols));
         }
         if (nickname.length() < 2 || nickname.length() > 16) {
-            return new Status(false, "Кличка должна содержать не менее 2 и не более 16 символов.");
+            return new Status(false, "Прозвище должна содержать не менее 2 и не более 16 символов.");
         }
         return new Status(true);
     }
@@ -77,10 +86,12 @@ public class UserValidator {
         if (!StringUtils.hasText(password)) {
             return new Status(false, "Пароль не может быть пустым.");
         }
-        if (!password.matches(PASSWORD_VALIDATION_REGEX)) {
-            return new Status(false, "Пароль содержит запрещённые символы.");
+        Collection<String> defectSymbols = checkStringSymbolsByRegexp(password, PASSWORD_VALIDATION_REGEX);
+        if (!defectSymbols.isEmpty()) {
+            return new Status(false,
+                    "Пароль содержит запрещённые символы: " + String.join(", ", defectSymbols));
         }
-        if (password.length() < 8) {
+        if (password.length() < 8 || password.length() > 24) {
             return new Status(false, "Пароль должен содержать не менее 8 символов.");
         }
         return new Status(true);
@@ -97,5 +108,19 @@ public class UserValidator {
             return new Status(false, "Пользователь с таким e-mail уже существует.");
         }
         return new Status(true);
+    }
+
+    private Collection<String> checkStringSymbolsByRegexp(String str, String regexp) {
+        if (!StringUtils.hasText(str)) {
+            return Collections.emptyList();
+        }
+        Collection<String> result = new ArrayList<>();
+        for (char chr : str.toCharArray()) {
+            String chrOfStr = Character.toString(chr);
+            if (!chrOfStr.matches(regexp)) {
+                result.add(chrOfStr);
+            }
+        }
+        return result;
     }
 }
