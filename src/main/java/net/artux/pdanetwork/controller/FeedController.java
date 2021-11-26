69dd5f325564272205810d894412c07f8685238f
Semyon Kulikov <senich.99@gmail.com>
2021-11-26 20:17:28 +0300

    Changed article date format

--- src/main/java/net/artux/pdanetwork/controller/FeedController.java
+++ src/main/java/net/artux/pdanetwork/controller/FeedController.java
@@ -4,13 +4,17 @@ import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import net.artux.pdanetwork.models.QueryPage;
 import net.artux.pdanetwork.models.ResponsePage;
-import net.artux.pdanetwork.service.feed.FeedService;
 import net.artux.pdanetwork.models.feed.Article;
+import net.artux.pdanetwork.service.feed.FeedService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller
@@ -19,23 +23,24 @@ import java.util.Date;
 @RequestMapping("/feed")
 public class FeedController {
 
-  private final FeedService feedService;
+    private final FeedService feedService;
+    private static final SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
 
-  @ResponseBody
-  @GetMapping
-  public ResponsePage<Article> getPageArticles(@Valid QueryPage page){
-    return feedService.getPageArticles(page);
-  }
+    @ResponseBody
+    @GetMapping
+    public ResponsePage<Article> getPageArticles(@Valid QueryPage page) {
+        return feedService.getPageArticles(page);
+    }
 
-  @GetMapping("/{id}")
-  public String getArticlePage(Model model, @PathVariable String id){
-    Article article = feedService.getArticle(id);
-    model.addAttribute("title", article.getTitle());
-    model.addAttribute("content", article.getContent());
-    model.addAttribute("tags", article.getTags());
-    model.addAttribute("date", new Date(article.getPublished()).toString());
-    model.addAttribute("comments", 0);
-    return "article";
-  }
+    @GetMapping("/{id}")
+    public String getArticlePage(Model model, @PathVariable String id) {
+        Article article = feedService.getArticle(id);
+        model.addAttribute("title", article.getTitle());
+        model.addAttribute("content", article.getContent());
+        model.addAttribute("tags", article.getTags());
+        model.addAttribute("date", df.format(new Date(article.getPublished())));
+        model.addAttribute("comments", 0);
+        return "article";
+    }
 
 }
