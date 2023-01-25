af75012febce7ab9f233b0ebfb93091242325d1e
Maxim <maksim.prygunov@gmail.com>
2023-01-25 15:54:39 +0400

    items container fix

--- src/main/java/net/artux/pdanetwork/controller/rest/items/ItemsController.java
+++ src/main/java/net/artux/pdanetwork/controller/rest/items/ItemsController.java
@@ -3,53 +3,24 @@ package net.artux.pdanetwork.controller.rest.items;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import net.artux.pdanetwork.entity.items.ItemType;
-import net.artux.pdanetwork.models.Status;
-import net.artux.pdanetwork.models.seller.SellerDto;
-import net.artux.pdanetwork.service.items.SellerService;
-import org.springframework.web.bind.annotation.*;
+import net.artux.pdanetwork.models.items.ItemsContainer;
+import net.artux.pdanetwork.service.items.ItemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
+@Tag(name = "Предметы")
 @RestController
+@RequestMapping("/items")
 @RequiredArgsConstructor
-@Tag(name = "Продавцы")
-@RequestMapping("/sellers")
 public class ItemsController {
 
-    private final SellerService sellerService;
+    private final ItemService itemService;
 
-    @Operation(summary = "Получить продавца")
-    @GetMapping("/{id}")
-    public SellerDto getSeller(@PathVariable long id) {
-        return sellerService.getSeller(id);
-    }
-
-    @Operation(summary = "Операция с предметом")
-    @PostMapping("/{sellerId}/{type}/{itemId}")
-    public Status actionWithItem(@PathVariable OperationType type, @PathVariable long sellerId,
-                                 @PathVariable UUID itemId, int quantity) {
-        return switch (type) {
-            case BUY -> sellerService.buy(sellerId, itemId, quantity);
-            case SELL -> sellerService.sell(sellerId, itemId, quantity);
-        };
-    }
-
-    @Operation(summary = "Сделать предмет по умолчанию")
-    @PostMapping("/set/{itemId}")
-    public Status setItem(@PathVariable UUID itemId, ItemType type) {
-        return sellerService.set(type, itemId);
-    }
-
-    @Operation(summary = "Добавить предмет (только для админа)")
-    @PostMapping("/add")
-    public Status addItem(UUID pdaId, int id, int quantity) {
-        return sellerService.add(pdaId, id, quantity);
-    }
-
-    enum OperationType {
-        BUY,
-        SELL
+    @Operation(summary = "Получить все предметы по типам")
+    @GetMapping("/all")
+    public ItemsContainer getContainer() {
+        return itemService.getItemsContainer();
     }
 
 }
---
+++ src/main/java/net/artux/pdanetwork/controller/rest/items/SellerController.java
@@ -0,0 +1,55 @@
+package net.artux.pdanetwork.controller.rest.items;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import net.artux.pdanetwork.entity.items.ItemType;
+import net.artux.pdanetwork.models.Status;
+import net.artux.pdanetwork.models.seller.SellerDto;
+import net.artux.pdanetwork.service.items.SellerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Продавцы")
+@RequestMapping("/sellers")
+public class SellerController {
+
+    private final SellerService sellerService;
+
+    @Operation(summary = "Получить продавца")
+    @GetMapping("/{id}")
+    public SellerDto getSeller(@PathVariable long id) {
+        return sellerService.getSeller(id);
+    }
+
+    @Operation(summary = "Операция с предметом")
+    @PostMapping("/{sellerId}/{type}/{itemId}")
+    public Status actionWithItem(@PathVariable OperationType type, @PathVariable long sellerId,
+                                 @PathVariable UUID itemId, int quantity) {
+        return switch (type) {
+            case BUY -> sellerService.buy(sellerId, itemId, quantity);
+            case SELL -> sellerService.sell(sellerId, itemId, quantity);
+        };
+    }
+
+    @Operation(summary = "Сделать предмет по умолчанию")
+    @PostMapping("/set/{itemId}")
+    public Status setItem(@PathVariable UUID itemId, ItemType type) {
+        return sellerService.set(type, itemId);
+    }
+
+    @Operation(summary = "Добавить предмет (только для админа)")
+    @PostMapping("/add")
+    public Status addItem(UUID pdaId, int id, int quantity) {
+        return sellerService.add(pdaId, id, quantity);
+    }
+
+    enum OperationType {
+        BUY,
+        SELL
+    }
+
+}
