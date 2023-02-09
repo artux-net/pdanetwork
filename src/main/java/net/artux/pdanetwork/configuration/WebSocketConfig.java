b45cfefbd9aeffca38c3c6cb1c36d942bffbb2ab
Maxim <maksim.prygunov@gmail.com>
2023-02-09 23:56:45 +0400

    implement chat

--- src/main/java/net/artux/pdanetwork/configuration/WebSocketConfig.java
+++ src/main/java/net/artux/pdanetwork/configuration/WebSocketConfig.java
@@ -5,6 +5,7 @@ import net.artux.pdanetwork.configuration.handlers.ChatHandler;
 import net.artux.pdanetwork.configuration.handlers.DialogsHandler;
 import net.artux.pdanetwork.configuration.handlers.GroupsHandler;
 import net.artux.pdanetwork.configuration.handlers.MessagesHandler;
+import net.artux.pdanetwork.configuration.handlers.RPHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -19,6 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private final MessagesHandler messagesHandler;
     private final DialogsHandler dialogsHandler;
     private final GroupsHandler groupsHandler;
+    private final RPHandler rpHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
@@ -27,7 +29,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
                 .addHandler(messagesHandler, "/dialog")
                 .addHandler(dialogsHandler, "/dialogs")
                 .addHandler(groupsHandler, "/groups")
-                .addHandler(groupsHandler, "/rp")
+                .addHandler(rpHandler, "/rp")
                 .setAllowedOriginPatterns("*")
                 .setAllowedOrigins("*");
     }
