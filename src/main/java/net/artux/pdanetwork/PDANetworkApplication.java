2c8c05f62ba2a9e89f2a2db8aba951aa64a92d59
Maxim <maksim.prygunov@gmail.com>
2023-05-06 11:33:45 +0400

    disable cors

--- src/main/java/net/artux/pdanetwork/PDANetworkApplication.java
+++ src/main/java/net/artux/pdanetwork/PDANetworkApplication.java
@@ -6,16 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.spring5.ISpringTemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
-@EnableWebMvc
 @Configuration
 public class PDANetworkApplication {
 
@@ -38,16 +34,6 @@ public class PDANetworkApplication {
         engine.addDialect(new Java8TimeDialect());
         engine.setTemplateResolver(templateResolver);
         return engine;
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
     }
 
 }
